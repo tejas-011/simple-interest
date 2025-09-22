0ab21e2a2a5c23b71e684a725ccbf96394938eb7
tejas-011 <141647567+tejas-011@users.noreply.github.com>
2025-09-22 10:07:06 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# Script to calculate Simple Interest
 
+echo "Enter Principal:"
+read p
+
+echo "Enter Rate of Interest:"
+read r
+
+echo "Enter Time (in years):"
+read t
+
+si=$(( (p * r * t) / 100 ))
+
+echo "Simple Interest = $si"
